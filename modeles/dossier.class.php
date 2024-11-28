7f2644cc0b49bbb63e62031a5de7166963c89a60
Ugo Bergos <ugo.bergos@gmail.com>
2024-11-28 09:01:59 +0100

    Ajout commentaires modeles classe dossier et utilisateur

--- modeles/dossier.class.php
+++ modeles/dossier.class.php
@@ -1,7 +1,10 @@
 <?php
 
+/**
+ * @file dossier.class.php
+ * @brief Classe Dossier
+ */
 class Dossier {
-
     private ?int $id;
     private ?string $identiteRecto;
     private ?string $identiteVerso;
@@ -9,6 +12,15 @@ class Dossier {
     private ?string $cv;
     private ?int $idCoach;
 
+    /**
+     * @brief Constructeur de la classe Dossier
+     * @param int|null $id Identifiant du dossier
+     * @param string|null $identiteRecto Chemin du fichier de l'identité recto
+     * @param string|null $identiteVerso Chemin du fichier de l'identité verso
+     * @param string|null $certificat Chemin du fichier du certificat
+     * @param string|null $cv Chemin du fichier du CV
+     * @param int|null $idCoach Identifiant du coach
+     */
     public function __construct(
         ?int $id = null, 
         ?string $identiteRecto = null,
@@ -25,66 +37,99 @@ class Dossier {
         $this->idCoach = $idCoach;
     }
 
+    /**
+     * Getter de la variable membre id
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * Setter de la variable membre id
+     */
     public function setId(?int $id): void
     {
         $this->id = $id;
     }
 
+    /**
+     * Getter de la variable membre identiteRecto
+     */
     public function getIdentiteRecto(): ?string
     {
         return $this->identiteRecto;
     }
 
+    /**
+     * Setter de la variable membre identiteRecto
+     */
     public function setIdentiteRecto(?string $identiteRecto): void
     {
         $this->identiteRecto = $identiteRecto;
     }
 
+    /**
+     * Getter de la variable membre identiteVerso
+     */
     public function getIdentiteVerso(): ?string
     {
         return $this->identiteVerso;
     }
 
+    /**
+     * Setter de la variable membre identiteVerso
+     */
     public function setIdentiteVerso(?string $identiteVerso): void
     {
         $this->identiteVerso = $identiteVerso;
     }
 
+    /**
+     * Getter de la variable membre certificat
+     */
     public function getCertificat(): ?string
     {
         return $this->certificat;
     }
 
+    /**
+     * Setter de la variable membre certificat
+     */
     public function setCertificat(?string $certificat): void
     {
         $this->certificat = $certificat;
     }
 
+    /**
+     * Getter de la variable membre cv
+     */
     public function getCv(): ?string
     {
         return $this->cv;
     }
 
+    /**
+     * Setter de la variable membre cv
+     */
     public function setCv(?string $cv): void
     {
         $this->cv = $cv;
     }
 
+    /**
+     * Getter de la variable membre idCoach
+     */
     public function getIdCoach(): ?int
     {
         return $this->idCoach;
     }
 
+    /**
+     * Setter de la variable membre idCoach
+     */
     public function setIdCoach(?int $idCoach): void
     {
         $this->idCoach = $idCoach;
     }
-
-
-
 }
--- modeles/utilisateur.class.php
+++ modeles/utilisateur.class.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @file utilisateur.class.php
+ * @brief Classe Utilisateur
+ */
 class Utilisateur {
     private ?int $id;
     private ?string $motDePasse;
@@ -11,6 +15,18 @@ class Utilisateur {
     private ?bool $estActif;
     private ?bool $estAdmin;
 
+    /**
+     * @brief Constructeur de la classe Utilisateur
+     * @param int|null $id Identifiant de l'utilisateur
+     * @param string|null $motDePasse Mot de passe de l'utilisateur
+     * @param string|null $nom Nom de l'utilisateur
+     * @param string|null $prenom Prénom de l'utilisateur
+     * @param string|null $mail Mail de l'utilisateur
+     * @param string|null $photo Photo de l'utilisateur
+     * @param DateTime|null $dateInscription Date d'inscription de l'utilisateur
+     * @param bool|null $estActif Si l'utilisateur est actif
+     * @param bool|null $estAdmin Si l'utilisateur est admin
+     */
     public function __construct(
         ?int $id = null,
         ?string $motDePasse = null,
@@ -33,95 +49,148 @@ class Utilisateur {
         $this->estAdmin = $estAdmin;
     }
 
+    /**
+     * Getter de la variable membre id
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * Setter de la variable membre id
+     */
     public function setId(?int $id): void
     {
         $this->id = $id;
     }
 
+    /**
+     * Getter de la variable membre motDePasse
+     */
     public function getMotDePasse(): ?string
     {
         return $this->motDePasse;
     }
 
+    /**
+     * Setter de la variable membre motDePasse
+     */
     public function setMotDePasse(?string $motDePasse): void
     {
         $this->motDePasse = $motDePasse;
     }
 
+    /**
+     * Getter de la variable membre nom
+     */
     public function getNom(): ?string
     {
         return $this->nom;
     }
 
+    /**
+     * Setter de la variable membre nom
+     */
     public function setNom(?string $nom): void
     {
         $this->nom = $nom;
     }
 
+    /**
+     * Getter de la variable membre prenom
+     */
     public function getPrenom(): ?string
     {
         return $this->prenom;
     }
 
+    /**
+     * Setter de la variable membre prenom
+     */
     public function setPrenom(?string $prenom): void
     {
         $this->prenom = $prenom;
     }
 
+    /**
+     * Getter de la variable membre mail
+     */
     public function getMail(): ?string
     {
         return $this->mail;
     }
 
+    /**
+     * Setter de la variable membre mail
+     */
     public function setMail(?string $mail): void
     {
         $this->mail = $mail;
     }
 
+    /**
+     * Getter de la variable membre photo
+     */
     public function getPhoto(): ?string
     {
         return $this->photo;
     }
 
+    /**
+     * Setter de la variable membre photo
+     */
     public function setPhoto(?string $photo): void
     {
         $this->photo = $photo;
     }
 
+    /**
+     * Getter de la variable membre dateInscription
+     */
     public function getDateInscription(): ?DateTime
     {
         return $this->dateInscription;
     }
 
+    /**
+     * Setter de la variable membre dateInscription
+     */
     public function setDateInscription(?DateTime $dateInscription): void
     {
         $this->dateInscription = $dateInscription;
     }
 
+    /**
+     * Getter de la variable membre estActif
+     */
     public function getEstActif(): ?bool
     {
         return $this->estActif;
     }
 
+    /**
+     * Setter de la variable membre estActif
+     */
     public function setEstActif(?bool $estActif): void
     {
         $this->estActif = $estActif;
     }
 
+    /**
+     * Getter de la variable membre estAdmin
+     */
     public function getEstAdmin(): ?bool
     {
         return $this->estAdmin;
     }
 
+    /**
+     * Setter de la variable membre estAdmin
+     */
     public function setEstAdmin(?bool $estAdmin): void
     {
         $this->estAdmin = $estAdmin;
     }
-
 }
 
