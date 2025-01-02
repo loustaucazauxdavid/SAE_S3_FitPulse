a9edf69578300e50c2171ae29086dd2ec23f568c
Manon-KP <manon.kleinpol12@gmail.com>
2025-01-02 12:52:02 +0100

    Correction des noms de tables dans les constantes
    Il manquait une majuscule à certaines tables (Creneau, Discipline, Dossier, Pratiquant, Salle_de_sport, Seance et Utilisateurs)

--- config/constantes.php
+++ config/constantes.php
@@ -14,15 +14,15 @@ define('DB_PASS', 'fitpulse'); /// Mot de passe
 define('PREFIXE_TABLE', 'fitpulse_'); /// Préfixe des tables : Nom de la base de données
 define('TABLE_COMMENTER', PREFIXE_TABLE . "commenter"); /// Table commenter
 define('TABLE_COACH', PREFIXE_TABLE . "Coach"); /// Table Coach
-define('TABLE_CRENEAU', PREFIXE_TABLE . "creneau"); /// Table creneau
-define('TABLE_DISCIPLINE', PREFIXE_TABLE . "discipline"); /// Table discipline
-define('TABLE_DOSSIER', PREFIXE_TABLE . "dossier"); /// Table dossier
-define('TABLE_PRATIQUANT', PREFIXE_TABLE . "pratiquant"); /// Table pratiquant
+define('TABLE_CRENEAU', PREFIXE_TABLE . "Creneau"); /// Table creneau
+define('TABLE_DISCIPLINE', PREFIXE_TABLE . "Discipline"); /// Table discipline
+define('TABLE_DOSSIER', PREFIXE_TABLE . "Dossier"); /// Table dossier
+define('TABLE_PRATIQUANT', PREFIXE_TABLE . "Pratiquant"); /// Table pratiquant
 define('TABLE_PRATIQUER', PREFIXE_TABLE . "pratiquer"); /// Table pratiquer
 define('TABLE_PROPOSER_DANS', PREFIXE_TABLE . "proposer_dans"); /// Table proposer_dans
-define('TABLE_SALLE_DE_SPORT', PREFIXE_TABLE . "salle_de_sport"); /// Table salle_de_sport
-define('TABLE_SEANCE', PREFIXE_TABLE . "seance"); /// Table seance
-define('TABLE_UTILISATEUR', PREFIXE_TABLE . "utilisateur"); /// Table utilisateur
+define('TABLE_SALLE_DE_SPORT', PREFIXE_TABLE . "Salle_de_sport"); /// Table salle_de_sport
+define('TABLE_SEANCE', PREFIXE_TABLE . "Seance"); /// Table seance
+define('TABLE_UTILISATEUR', PREFIXE_TABLE . "Utilisateur"); /// Table utilisateur
 
 // Constante de la partie vue
 define('WEBSITE_TITLE', 'FitPulse'); /// Titre du site
