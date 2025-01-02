a52c853703d963ea95319ced438f946325642eca
Manon-KP <manon.kleinpol12@gmail.com>
2025-01-02 14:43:56 +0100

    git commit -m "Template recherche - Mise à jour du contrôleur avec l'affichage des coachs disponibles"

--- controllers/controller_coach.class.php
+++ controllers/controller_coach.class.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * @file controller_coach.class.php
  * @brief Controller pour les coachs
@@ -8,22 +9,25 @@
  * @brief Controller pour les coachs
  * @details Gère les actions liées aux coachs
  */
-class ControllerCoach extends Controller{
+class ControllerCoach extends Controller
+{
     /**
      * Constructeur de la classe ControllerCoach
      * @param \Twig\Environment $twig
      * @param \Twig\Loader\FilesystemLoader $loader
      * @return void
      */
-    public function __construct(\Twig\Environment $twig, \Twig\Loader\FilesystemLoader $loader) {
+    public function __construct(\Twig\Environment $twig, \Twig\Loader\FilesystemLoader $loader)
+    {
         parent::__construct($twig, $loader);
-    }  
+    }
 
     /**
      * @brief Liste les coachs
      * @return void
      */
-    public function lister(){
+    public function lister()
+    {
         // Recupération de tous les coachs (Test avant de lister les coachs par notes)
         $managerCoach = new CoachNoteDao($this->getPdo());
         $listeCoachsNotesAssoc = $managerCoach->findTopNbByNote(10);
@@ -37,5 +41,23 @@ class ControllerCoach extends Controller{
             'listeCoachsNotes' => $listeCoachsNotes,
             'estConnecte' => false // TODO : Implémenter système de connexion et passer la variable dans les controlleurs
         ));
+    }
+
+    public function displayAvailableCoachs()
+    {
+        // Récupérer la liste des coachs ayant des séances disponibles
+        $managerCoach = new CoachDao($this->getPdo()); // Utilisation du modèle CoachDao
+        $coachs = $managerCoach->findAvailableCoachs(); // Trouver les coachs disponibles
+
+        // Charger le template 'rechercher.html.twig'
+        $template = $this->getTwig()->load('rechercher.html.twig');
+
+        // Passer les données à Twig pour affichage
+        echo $template->render([
+            'menu' => 'Recherche',
+            'description' => 'Page de recherche pour FitPulse',
+            'estConnecte' => false, // Change à true si l'utilisateur est connecté
+            'coachs' => $coachs,    // Liste des coachs avec leurs séances et notes
+        ]);
     }
 }
