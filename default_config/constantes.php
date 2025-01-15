ef9f38c5ff58a564ddf6886eb92ec3b6720e521a
loustaucazauxdavid <dlcazaux@iutbayonne.univ-pau.fr>
2025-01-15 01:12:55 +0100

    Ajout des fichiers de configuration par défaut

---
+++ default_config/constantes.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file constantes.php
+ * @brief Fichier contenant les constantes du site
+ */
+
+// Chemin absolu vers le dossier racine du projet. Par défaut, le dossier parent du dossier config.
+define('APP_ROOT', dirname(__DIR__));  
+?>
